b8e61d1a3917a4d6dcea1a9b517e7fe9d9ef233d
Lixiang Avila <lixiangrivera@gmail.com>
2022-10-17 11:05:06 -0500

    Iniciando repositorio

---
+++ algo_de_estuctura_de_datos_para_estudiar/src/algo_de_estuctura_de_datos_para_estudiar/Algo_de_estuctura_de_datos_para_estudiar.java
@@ -0,0 +1,11 @@
+
+package algo_de_estuctura_de_datos_para_estudiar;
+
+
+public class Algo_de_estuctura_de_datos_para_estudiar {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
