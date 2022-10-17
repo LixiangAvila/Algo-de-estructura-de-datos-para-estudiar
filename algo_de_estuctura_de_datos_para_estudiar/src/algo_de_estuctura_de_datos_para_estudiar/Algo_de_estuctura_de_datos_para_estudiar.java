211cf12fc37da6d506941e8a81cc14b2cf7eba2a
Lixiang Avila <lixiangrivera@gmail.com>
2022-10-17 11:06:34 -0500

    Revisando repositorio

--- algo_de_estuctura_de_datos_para_estudiar/src/algo_de_estuctura_de_datos_para_estudiar/Algo_de_estuctura_de_datos_para_estudiar.java
+++ algo_de_estuctura_de_datos_para_estudiar/src/algo_de_estuctura_de_datos_para_estudiar/Algo_de_estuctura_de_datos_para_estudiar.java
@@ -5,7 +5,7 @@ package algo_de_estuctura_de_datos_para_estudiar;
 public class Algo_de_estuctura_de_datos_para_estudiar {
 
     public static void main(String[] args) {
-        
+        System.out.println("sqwtryeiñop");
     }
     
 }
