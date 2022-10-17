cf70b1c202a25cc6bc035fa0496e995f40a51b38
Lixiang Avila <lixiangrivera@gmail.com>
2022-10-17 12:18:02 -0500

    Puse una clase vacia cola llamada, un scanner de teclado en main

--- algo_de_estuctura_de_datos_para_estudiar/src/algo_de_estuctura_de_datos_para_estudiar/Algo_de_estuctura_de_datos_para_estudiar.java
+++ algo_de_estuctura_de_datos_para_estudiar/src/algo_de_estuctura_de_datos_para_estudiar/Algo_de_estuctura_de_datos_para_estudiar.java
@@ -1,11 +1,33 @@
 
 package algo_de_estuctura_de_datos_para_estudiar;
 
+import java.util.Scanner;
+
+class Cola {
+    
+}
 
 public class Algo_de_estuctura_de_datos_para_estudiar {
-
-    public static void main(String[] args) {
-        System.out.println("sqwtryeiñop");
-    }
     
+    static char menu(Scanner input)
+    {
+        char opcion;
+        System.out.println("Elige algo");
+        opcion=input.nextLine().charAt(0);
+        return opcion;
+    }
+    public static void main(String[] args) {
+
+        char opc;
+        
+        Scanner teclado;
+        
+        teclado=new Scanner(System.in);
+        int max=teclado.nextInt();
+        
+        teclado.nextLine(); // saca la cantidad ingresada
+        
+        opc=menu(teclado);
+                
+    }
 }
