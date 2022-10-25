239c2314dcac816044fd0576fdf91c4464f915fd
Lixiang Avila <lixiangrivera@gmail.com>
2022-10-24 22:41:35 -0500

    iniciando version con nodos

---
+++ algo_de_estructura_de_datos_con_nodos/src/algo_de_estructura_de_datos_para_estudiar_con_nodos/Algo_de_estructura_de_datos_para_estudiar_con_nodos.java
@@ -0,0 +1,11 @@
+package algo_de_estructura_de_datos_para_estudiar_con_nodos;
+
+import java.util.Scanner;
+
+public class Algo_de_estructura_de_datos_para_estudiar_con_nodos {
+
+
+    public static void main(String[] args) {
+    }
+    
+}
